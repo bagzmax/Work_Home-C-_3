4f7e83b3f78a135917fc8f07e28f79ab72a9c85e
Maksim <bagzmax75@gmail.com>
2022-07-06 01:00:58 +0300

    add work home and primer

---
+++ Primer/Program.cs
@@ -0,0 +1,39 @@
+﻿//var v = 12321;
+// var s = v.ToString();
+// bool IsPalindrome(string s);
+// for (int i = 0; i < s.Length / 2; ++i)
+// {
+//     if (s[i] != s[s.Length - 1 - i])
+//     {
+//         return false;
+//     }    
+//     return true;
+// }
+// System.Console.WriteLine(IsPalindrome);
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+foreach (int i in numbers)
+{
+    Console.Write(i + " ");
+}
+
+string text = Console.ReadLine();
+int len = text.Length;
+bool flag = true;
+
+for (int i = 0; i < len/2; i++)
+{
+    if (text[i] != text[len - (i + 1)])
+    {
+        flag = false;
+        break;
+    }      
+}
+if (flag)
+{
+    Console.WriteLine("{0} is palindrome", text);
+}
+else
+{
+    Console.WriteLine("{0} is not palindrome", text);
+}
---
+++ WorkHom3_19/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+//14212 -> нет
+//23432 -> да
+
+Console.Write ("Введите число элементов массива: ");
+int Count = int.Parse (Console.ReadLine()!);
+Console.WriteLine();
+int[] array = new int [Count];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите {i}-й элемент массива: ");
+    array[i] = int.Parse(Console.ReadLine()!);
+}
+Console.Write("\nВывод массива: ");
+foreach (int i in array)
+{
+    Console.Write(i);
+}
+//for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write(array[i] + " ");
+// }
+//System.Console.WriteLine();
+//int [] array = {8, 7, 33, 55, 33, 7, 8};
+//int step = array.Length;
+//int j = step - 1;
+bool flag = true;
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+    if (array[i] != array[array.Length - 1 -i])
+    {
+       flag = false;
+       break;
+    } 
+}
+    if (flag)
+    {
+       Console.WriteLine("это палиндром");
+        //Console.WriteLine($"\nНет совпадения позиций с индексами {i} и {array.Length - 1 -i}" + $" - не палиндром");
+      //System.Console.WriteLine($"совпадение позиции с индексом {i} и {j}"); 
+      //j -=1; 
+    }
+    else
+    {    
+      Console.WriteLine("не палиндром");
+        //Console.WriteLine($"\nЕсть совпадение позиций с индексами {i} и {array.Length - 1 -i}" + $" - это палиндром");
+        //Console.WriteLine($"не совпали позиции с индексами {i} и {j}");
+        //j -=1;
+    }
+
+
