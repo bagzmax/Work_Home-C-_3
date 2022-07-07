18a9a7774f918ff33ecb126a0e5478b74db44480
Maksim <bagzmax75@gmail.com>
2022-07-07 22:20:41 +0300

    add primer

--- Primer/Program.cs
+++ Primer/Program.cs
@@ -1,10 +1,1 @@
-//Console.WriteLine ("Введите число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int i = 1;
-
-// while (Math.Abs(num) - i >=0) 
-//  {
-//      Console.Write ($"{i*i*i},");
-//      i ++;
-//  }
-//  Console.Write("\b \b");
+﻿
