6ef9363d79cd153a8e71724d055bea2cd1373704
Maksim <bagzmax75@gmail.com>
2022-07-07 12:16:01 +0300

    add final  work home 3.19

--- WorkHom3_19/Program.cs
+++ WorkHom3_19/Program.cs
@@ -3,27 +3,14 @@
 //14212 -> нет
 //23432 -> да
 
-Console.Write ("Введите пятизначное число: ");
-string number = Console.ReadLine()!;
-int len = number.Length;
 
-void CheckingNumber(string number)
+Console.Write("Введите пятизначное число:");
+string? number = Console.ReadLine();
+if (number!.Length == 5)
 {
-bool flag = true;
-
-for (int i = 0; i < len/2; i++) {
-    if (number[i] != number[len - (i + 1)]) {
-        flag = false;
-        break;
-    }      
-}
-if (flag) {
-    Console.WriteLine("{0} это палиндром", number);
-}
-else Console.WriteLine("{0} это не палиндром", number);
-}
-
-if (number!.Length == 5){
-  CheckingNumber(number);
+if (number[0] == number[4] && number[1] == number[3])
+{ System.Console.WriteLine("{0} это палиндром", number); }
+else
+{ System.Console.WriteLine("{0} это не палиндром", number); }  
 }
 else Console.WriteLine($"Введите правильное число");
