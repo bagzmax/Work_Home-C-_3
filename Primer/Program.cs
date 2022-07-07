adfcde5af3be420193a3ec85f37cfd88a5e316ae
Maksim <bagzmax75@gmail.com>
2022-07-07 22:17:20 +0300

    add work home 3.23

--- Primer/Program.cs
+++ Primer/Program.cs
@@ -1,40 +1,10 @@
-﻿//var v = 12321;
-// var s = v.ToString();
-// bool IsPalindrome(string s);
-// for (int i = 0; i < s.Length / 2; ++i)
-// {
-//     if (s[i] != s[s.Length - 1 - i])
-//     {
-//         return false;
-//     }    
-//     return true;
-// }
-// Console.WriteLine(IsPalindrome);
+//Console.WriteLine ("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
 
-//int[] numbers = { 1, 2, 3, 4, 5 };
-// foreach (int i in numbers)
-// {
-//     Console.Write(i + " ");
-// }
-
-Console.Write ("Введите число: ");
-string text = Console.ReadLine()!;
-int len = text.Length;
-bool flag = true;
-
-for (int i = 0; i < len/2; i++)
-{
-    if (text[i] != text[len - (i + 1)])
-    {
-        flag = false;
-        break;
-    }      
-}
-if (flag)
-{
-    Console.WriteLine("{0} это палиндром", text);
-}
-else
-{
-    Console.WriteLine("{0} это не палиндром", text);
-}
+// while (Math.Abs(num) - i >=0) 
+//  {
+//      Console.Write ($"{i*i*i},");
+//      i ++;
+//  }
+//  Console.Write("\b \b");
--- WorkHome_3.23/Program.cs
+++ WorkHome_3.23/Program.cs
@@ -1,14 +1,14 @@
-﻿// Напишите программу, которая принимает на вход число (N)
+// Напишите программу, которая принимает на вход число (N)
 // и выдаёт таблицу кубов чисел от 1 до N.
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
-Console.WriteLine ("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int i = 1;
 
-while (Math.Abs(num) - i >=0) 
- {
-     Console.Write ($"{i*i*i},");
-     i ++;
- }
- Console.Write("\b \b");
+Console.Write ("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{i*i*i}, ");
+}
+Console.Write("\b \b");
+Console.Write("\b \b");
